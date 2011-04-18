70ac09e613e62923b72eb8a9ecf818e2f94550a2
shikamu <tomy4ever@gmail.com>
2011-04-18 11:35:24 +0200

    added slotrange model EHEHEHEHEH

--- GR-Calcul/Models/SlotRangeModels.cs
+++ GR-Calcul/Models/SlotRangeModels.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GR_Calcul.Models
 {
     public class SlotRangeModels
     {
+        [Required]
+        [Display(Name = "Nom")]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        
+        [Required]
+        [Display(Name = "Début de Réservation")]
+        [DataType(DataType.Date)]
+        public DateTime StartRes { get; set; }
+        
+        [Required]
+        [Display(Name = "Fin de Réservation")]
+        [DataType(DataType.Date)]        
+        public DateTime EndRes { get; set; }
+
     }
+
+
 }
