f4b69b7b1c32e083b5cdc100b17e4a631d4eb257
shikamu <tomy4ever@gmail.com>
2011-05-03 08:33:26 +0200

    fixed a few bugs, basic implementation of permission checking

---
+++ GR-Calcul/Misc/DuffAuthorizeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GR_Calcul.Misc
+{
+    public enum UserRole { User, Responsible, ResourceManager };
+
+    public class DuffAuthorizeAttribute : FilterAttribute, IAuthorizationFilter
+    {
+        public DuffAuthorizeAttribute(params UserRole[] acceptedRoles)
+        {
+            _acceptedRoles = acceptedRoles;
+        }
+
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            GR_Calcul.Controllers.SlotRangeController.User currentUser = GR_Calcul.Controllers.SlotRangeController.User.GetCurrentUser;
+
+            if (!currentUser.IsInRole(_acceptedRoles))
+                throw new Exception("Access denied");
+        }
+
+        private readonly UserRole[] _acceptedRoles;
+
+    }
+
+}
