4e6e5fd283975ad828ff21017ac1bc87e1859007
david.dutch <david.dutch@gmail.com>
2011-05-16 13:05:23 +0200

    Exception handling in course module

---
+++ GR-Calcul/Misc/GrException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GR_Calcul.Misc
+{
+    public class GrException : Exception
+    {
+        public string UserMessage { get; set; }
+
+        public GrException(string userMessage)
+        {
+            this.UserMessage = userMessage;
+        }
+
+        public GrException(Exception e, string userMessage) : base("", e)
+        {
+            this.UserMessage = userMessage;
+        }
+    }
+}
