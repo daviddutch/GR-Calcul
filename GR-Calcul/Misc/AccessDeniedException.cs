4c90ce03549b995e1cf76e9dc4fbb8fa27dd78cd
david.dutch <david.dutch@gmail.com>
2011-05-17 14:51:25 +0200

    AccessDenied with a better handling, Nb machine is not shown when a reservation is done

---
+++ GR-Calcul/Misc/AccessDeniedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GR_Calcul.Misc
+{
+    public class AccessDeniedException : Exception
+    {
+
+    }
+}
